54f86928150c4ea6aa12bb208f6b313a0b66c0e5
saurabh643 <68461646+saurabh643@users.noreply.github.com>
2020-07-24 20:43:24 +0530

    Add files via upload

---
+++ lottery.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 20:22:32 2020
+
+@author: Saurabh
+"""
+#my SEcond small project 
+
+
+print("Chosse any number")
+num= int(input())
+if num<100:
+    print("Better Luck Next Time")
+elif num>100:
+    print("you win 1000$ lottery ticket")
